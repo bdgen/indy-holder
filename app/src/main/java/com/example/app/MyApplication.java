f28660b0f1f97d23eb6948db9d4cf453df884dd5
Hyuk56 <134380724+Hyuk56@users.noreply.github.com>
2023-07-14 15:49:49 +0900

    1

---
+++ app/src/main/java/com/example/app/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import android.app.Application;
+
+import kr.co.bdgen.indywrapper.IndyWrapper;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        IndyWrapper.init(this);
+    }
+}
