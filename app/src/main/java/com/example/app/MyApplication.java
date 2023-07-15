19d6691c58ab87fe9f45583ff98a294a9308cc4f
Hyuk56 <134380724+Hyuk56@users.noreply.github.com>
2023-07-15 09:20:19 +0900

    1

--- app/src/main/java/com/example/app/MyApplication.java
+++ app/src/main/java/com/example/app/MyApplication.java
@@ -2,13 +2,24 @@ package com.example.app;
 
 import android.app.Application;
 
+import org.hyperledger.indy.sdk.IndyException;
+import org.hyperledger.indy.sdk.pool.Pool;
+
 import kr.co.bdgen.indywrapper.IndyWrapper;
+import kr.co.bdgen.indywrapper.config.PoolConfig;
 
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
         IndyWrapper.init(this);
+        String poolName = PoolConfig.getPoole(this);
+        try {
+            Pool.openPoolLedger(poolName, "{}");
+        } catch (IndyException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
